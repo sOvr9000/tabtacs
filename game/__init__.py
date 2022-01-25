ca146f54fed16d7ca77dcd3a83a1c802336f4324
sOvr9000 <jordanthegeek@gmail.com>
2022-01-24 18:26:16 -0800

    Include taclib functions

--- game/__init__.py
+++ game/__init__.py
@@ -2,6 +2,6 @@ from .prototypes import load_soldier_prototypes
 from .enums import SoldierType
 from .tabletactics import TableTactics
 from .random_sim import RandomSim
-
+from .taclib import *
 
 
