4529c260d5746987c965a8ba2b450c3f8fb2dd34
sOvr9000 <jordanthegeek@gmail.com>
2022-01-24 18:34:52 -0800

    Add dql files

---
+++ dql/data.py
@@ -0,0 +1,86 @@
+
+import numpy as np
+from tabtacs import SoldierType
+
+
+
+def get_state(game):
+	return \
+		np.interp(
+			np.concatenate((game.board, game.placement_mask[:,:,np.newaxis]), axis=2),
+			(-1, (1, 1, 2, 4, 2, 1)),
+			(-1, 1)
+		), \
+		np.interp(
+			[game.last_action_position_x, game.last_action_position_y, game.current_steps_remaining],
+			((-1, -1, 0), (5, 5, 4)),
+			(-1, 1)
+		)
+
+def games_to_input(games):
+	inp_board = np.zeros((len(games), 6, 6, 6))
+	inp_extra = np.zeros((len(games), 3))
+	for i,game in enumerate(games):
+		inp_board[i], inp_extra[i] = get_state(game)
+	return [inp_board, inp_extra]
+
+def compute_score(game):
+	return game.get_soldiers_remaining(0) - game.get_soldiers_remaining(1)
+
+def simulate(games, actions):
+	scores = map(compute_score,games)
+	for i,(game,(a_func,a_args)) in enumerate(zip(games,actions)):
+		a_func(*a_args)
+		scores[i] = compute_score(game) - scores[i]
+	return scores # change in score
+
+def random_action(game):
+	va = list(game.valid_actions())
+	return va[np.random.randint(len(va))]
+
+
+def action_to_indices(action):
+	# Model output 0 is array of shape (6,6,11).
+	# 11 = 4 + 4 + 3, one layer for each movement/attack direction (4+4) or soldier type to place (3) on any given tile (6x6).
+	# Model output 1 is a float value indicating the Q-value of ending the turn.
+	a_func, a_args = action
+	afn = a_func.__name__
+	if afn == 'end_turn':
+		return 1,0
+	if afn == 'move_soldier':
+		return 0,(a_args[1],a_args[0],a_args[2])
+	if afn == 'attack_soldier':
+		return 0,(a_args[1],a_args[0],a_args[2]+4)
+	if afn == 'add_soldier':
+		return 0,(a_args[1],a_args[0],int(a_args[2])+8)
+
+def indices_to_action(game, indices):
+	# inverse of function above (game must be provided to return bound methods)
+	i,t = indices
+	if i == 1:
+		return game.end_turn,()
+	y,x,k = t
+	if k >= 8:
+		return game.add_soldier,(x,y,SoldierType(k-8))
+	if k >= 4:
+		return game.attack_soldier,(x,y,k-4)
+	return game.move_soldier,(x,y,k)
+
+def actions_to_indices(actions):
+	# vectorized action_to_indices()
+	dim0_indices, dim1_indices = zip(*(action_to_indices(action) for action in actions))
+	print(dim0_indices)
+	print(dim1_indices)
+	return np.array(dim0_indices, dtype=int), np.array(dim1_indices, dtype=int)
+
+def indices_to_actions(game, indices):
+	# vectorized indices_to_action()
+	dim0_indices, dim1_indices = indices
+	return np.array([
+		indices_to_action(game, i)
+		for i in zip(dim0_indices, dim1_indices)
+	], dtype=int)
+
+
+
+
---
+++ dql/models.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, Dropout, Conv2D, Concatenate, Flatten, Reshape, Conv2DTranspose
+from data import games_to_input
+
+
+def build_model():
+	input_board = Input((6,6,6))
+	input_extra = Input((3,))
+
+	x = Conv2D(256, (2,2), (1,1), 'valid', activation='relu')(input_board)
+	x = Conv2D(256, (2,2), (1,1), 'valid', activation='relu')(x)
+	c3x3 = Conv2D(128, (2,2), (1,1), 'valid', activation='relu')(x)
+	sub_output_board1 = Flatten()(c3x3)
+	x = Conv2D(128, (2,2), (1,1), 'valid', activation='relu')(c3x3)
+	sub_output_board2 = Flatten()(x)
+	x = Concatenate()((sub_output_board1, sub_output_board2, input_extra))
+
+	x = Dense(512, 'relu')(x)
+	x = Dense(256, 'relu')(x)
+	sub_hidden_output = Dense(256, 'relu')(x)
+	hidden_output = Dense(180, 'relu')(sub_hidden_output)
+
+	x = Reshape((3,3,20))(hidden_output)
+	x = Concatenate()((x, c3x3))
+	x = Conv2DTranspose(256, (2,2), (1,1), 'valid', dilation_rate = (2,2), activation='relu')(x)
+	output_board = Conv2D(11, (2,2), (1,1), 'same')(x)
+
+	x = Dense(256, 'relu')(sub_hidden_output)
+	x = Dense(256, 'relu')(x)
+	output_end_turn = Dense(1)
+
+	model = tf.keras.Model(inputs = [input_board, input_extra], outputs = [output_board, output_end_turn])
+	model.compile('adam', 'mse')
+	return model
+
+
+def model_predict(model, games):
+	return model.predict(games_to_input(games))
+
+
+# def generate_actions(model, games, epsilon):
+# 	pred = model_predict(model, games)
+# 	vas = [list(game.valid_actions()) for game in games]
+# 	r = np.random.random(len(va))
+# 	return [va[i] if r[i] < epsilon else va[np.random.randint(len(va))] for i in np.argmax(pred, axis=1)]
+
+
