88d139296c91bae3b489c9d456fe8740bf0c5359
sOvr9000 <jordanthegeek@gmail.com>
2022-01-28 01:34:52 -0800

    Add random board setup generation

--- game/__init__.py
+++ game/__init__.py
@@ -4,6 +4,6 @@ from .tabletactics import TableTactics
 from .random_sim import RandomSim
 from .replay import Replay, load_replays, count_replays
 from .taclib import *
-from .board_setups import get_board_setup, copy_board_setup, set_board_setup, \
-    random_obstacles, save_board_setups, load_board_setups, fix_board_setup
+from .board_setups import get_board_setup, copy_board_setup, set_board_setup, save_board_setups, load_board_setups, fix_board_setup, \
+    random_obstacles, random_soldiers, random_placement_space, random_board_setup
 
--- game/board_setups.py
+++ game/board_setups.py
@@ -1,8 +1,9 @@
 
 import json
+import numpy as np
 from copy import deepcopy
-from random import choice
 from .enums import SoldierType
+from .taclib import obstacles_to_str
 
 
 
@@ -29,7 +30,7 @@ def set_board_setup(name, setup):
 		raise ValueError(f'A board setup already exists under the name \'{name}\'')
 	board_setups[name] = setup
 
-def random_obstacles(board_size, num_obstacles = 6):
+def random_obstacles(board_size, num_obstacles, symmetric = False):
 	'''
 	Randomly generate obstacles for a given board size.
 	board_size is a tuple of the form (width, height).
@@ -61,15 +62,103 @@ def random_obstacles(board_size, num_obstacles = 6):
 			if y > 0:
 				flood_fill(x,y-1,arr)
 
-	while len(open_set) > 0 and total < num_obstacles:
-		p = choice(open_set)
+	def try_position(p):
 		open_set.remove(p)
 		obstacles[p[1]][p[0]] = True
 		if not is_connected():
 			obstacles[p[1]][p[0]] = False
-		total += 1
+			return False
+		return True
+
+	while len(open_set) > 0 and (total+2 < num_obstacles or not symmetric and total < num_obstacles):
+		p = open_set[np.random.randint(len(open_set))]
+		if not try_position(p):
+			continue
+		if symmetric:
+			# TODO: refactor so as to not repeat any code
+			rp = (board_size[0]-1-p[0], board_size[1]-1-p[1])
+			if not try_position(rp):
+				obstacles[p[1]][p[0]] = False
+				continue
+			total += 2
+		else:
+			total += 1
 
 	return obstacles
+
+def random_soldiers(soldiers_per_player, symmetric = False):
+	'''
+	Randomly generate a soldier configuration for each player.
+	soldiers_per_player must be a list of integers each no less than one.
+	If symmetric is True, then the same randomly generated configuration is used for all players.
+	'''
+
+	soldiers = []
+	num_types = len(list(iter(SoldierType)))
+	for num_soldiers in soldiers_per_player:
+		v = np.abs(np.random.normal(size=num_types-1))
+		v = (v * (num_soldiers - 1) / v.sum() + 0.5).astype(int)
+		while v.sum() > num_soldiers:
+			m = v.max()
+			indices = [i for i,_v in enumerate(v) if _v == m]
+			v[indices[np.random.randint(len(indices))]] -= 1
+		_soldiers = {
+			SoldierType(i+1): v
+			for i,v in enumerate(v)
+		}
+		if symmetric:
+			return [deepcopy(_soldiers) for _ in range(len(soldiers_per_player))]
+		soldiers.append(_soldiers)
+	return soldiers
+
+def random_placement_space(obstacles, positions_per_player):
+	'''
+	Given a board's obstacles, randomly generate a placement space (a list of positions) for each player.
+	
+	positions_per_player must be a list of integers, each representing the number of positions available for a unique player.
+	'''
+	valid_positions = [(x,y) for y in range(len(obstacles)) for x in range(len(obstacles[0])) if not obstacles[y][x]]
+	if len(valid_positions) < sum(positions_per_player):
+		raise ValueError(f'The number of open positions in the given board is less than the total number of placement positions.\nBoard obstacles:\n{obstacles_to_str(obstacles)}\nPositions per player: {positions_per_player}')
+	return [
+		[
+			valid_positions.pop(np.random.randint(len(valid_positions)))
+			for _ in range(num_positions)
+		]
+		for num_positions in positions_per_player
+	]
+
+def random_board_setup(
+	board_size,
+	num_obstacles = 6,
+	num_players = 2,
+	positions_per_player = 4,
+	symmetric_obstacles = False,
+	soldiers_per_player = 4,
+	symmetric_soldiers = False
+):
+	'''
+	Randomly generate a board setup with the specified parameters.
+	'''
+	obstacles = random_obstacles(board_size, num_obstacles, symmetric=symmetric_obstacles)
+	placement_space = random_placement_space(
+		obstacles,
+		[positions_per_player]*num_players
+		if isinstance(positions_per_player, int) else
+		positions_per_player
+	)
+	soldiers = random_soldiers(
+		[soldiers_per_player]*num_players
+		if isinstance(soldiers_per_player, int) else
+		soldiers_per_player,
+		symmetric=symmetric_soldiers
+	)
+	return {
+		'board_size': board_size,
+		'placement_space': placement_space,
+		'soldiers': soldiers,
+		'obstacles': obstacles,
+	}
 
 def fix_board_setup(setup):
 	'''
