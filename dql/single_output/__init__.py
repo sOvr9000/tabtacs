873df7bd978dcfb6dc15b48d4016eeeb6c1ae0d3
sOvr9000 <jordanthegeek@gmail.com>
2022-01-28 21:15:56 -0800

    Adjust import

--- dql/single_output/__init__.py
+++ dql/single_output/__init__.py
@@ -3,7 +3,7 @@ from .data import get_state, games_to_input, heuristic_score, simulate, random_a
     action_to_indices, indices_to_action, actions_to_indices, indices_to_actions, \
     pred_argmax
 
-from .models import build_model, model_predict, predict_actions
+from .models import build_model, model_predict, predict_actions, load_model
 
 from .train import train_model
 
