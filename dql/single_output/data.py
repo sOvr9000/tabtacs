Fix terminal reward calculation

dql/single_output/data.py
@@ -25,7 +25,7 @@ def get_state(game, turn=None):
 	lal[game.last_action_location_y, game.last_action_location_x] = game.current_steps_remaining
 	return np.concatenate((
 		conc[:,:,[0]],
-		game.army_cycle[game.turn][conc[:,:,[1]].astype(int)],
+		game.army_cycle[turn][conc[:,:,[1]].astype(int)],
 		conc[:,:,[5]],
 		np.interp(conc[:,:,[2,4]], (-1, 2), (-1, 1)),
 		np.interp(conc[:,:,[3]], (-1, 4), (-1, 1)),